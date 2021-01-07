e0659c6e8b471e52825fb30188bbd29034f14cbe
hasmar04 <harrisonasmar@gmail.com>
2021-01-07 15:00:23 +1000

    Added extra commands for testing

--- GxEPD2_MinimumExample/GxEPD2_MinimumExample.ino
+++ GxEPD2_MinimumExample/GxEPD2_MinimumExample.ino
@@ -1,11 +1,13 @@
 // GxEPD2_MinimumExample.ino by Jean-Marc Zingg
 
 // uncomment next line to use class GFX of library GFX_Root instead of Adafruit_GFX
-#include <Adafruit_GFX.h>
+//#include <Adafruit_GFX.h>
 
 #define ENABLE_GxEPD2_GFX 0
 #include <GxEPD2_BW.h> // including both doesn't hurt
 #include <GxEPD2_3C.h> // including both doesn't hurt
+
+#include <Fonts/FreeMonoBold24pt7b.h> //Font
 
 // copy constructor for your e-paper from GxEPD2_Example.ino, and for AVR needed #defines
 #define MAX_DISPLAY_BUFFER_SIZE 800 // 
@@ -18,6 +20,8 @@ GxEPD2_BW<GxEPD2_270, GxEPD2_270::HEIGHT> display(GxEPD2_270(/*CS=D8*/ 2, /*DC=D
 void setup()
 {
   display.init();
+  //display.hibernate(); //No idea what this does
+  display.setRotation(3); //Sets the display to be the right orientation. 
   // comment out next line to have no or minimal Adafruit_GFX code
   display.setTextColor(GxEPD_BLACK);
   display.firstPage();
@@ -25,10 +29,15 @@ void setup()
   {
     display.fillScreen(GxEPD_WHITE);
     // comment out next line to have no or minimal Adafruit_GFX code
-    display.print("Hello World!");
-    Serial.write("Hello");
+    display.setCursor(display.width()/2,display.height()/2);
+    display.print("Hello World!!");
+    display.setCursor(0,50);
+    display.setFont(&FreeMonoBold24pt7b);
+    display.print("Hello again!");
+    display.setCursor(264,176);
+    display.print(".");
   }
   while (display.nextPage());
 }
 
-void loop() {};
+void loop() {}
